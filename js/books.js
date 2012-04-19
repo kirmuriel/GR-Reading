f30c50776475cb7a63155a52fb4d7b02dc970481
kirmuriel <kirmuriel@gmail.com>
2012-04-19 03:04:23 -0500

    Reformat

--- js/books.js
+++ js/books.js
@@ -44,7 +44,7 @@ jQuery(document).ready(function () {
 	});
 });
 
-function draw(allData){
+function draw(allData) {
 	var data = new google.visualization.DataTable();
 	var divId = 'mysuperdiv';
 	var i, hash, points;
@@ -63,13 +63,13 @@ function draw(allData){
 	data.addRows(points);
 
 	var options = {
-		width: 1200,
-		height: 200,
-		titleTextStyle: {color: 'black'},
-		hAxis: {gridlines: {count: 10 }, minValue: 0, maxValue: maxValue },
-		vAxis: {minValue: 0, maxValue: 100 },
-		lineWidth: 1,
-		legend: 'none'
+		width:1200,
+		height:200,
+		titleTextStyle:{color:'black'},
+		hAxis:{gridlines:{count:10 }, minValue:0, maxValue:100 },
+		vAxis:{minValue:0, maxValue:100 },
+		lineWidth:1,
+		legend:'none'
 	};
 
 	var chart = new google.visualization.LineChart(document.getElementById(divId));
@@ -80,10 +80,10 @@ function drawData(bookHash, response) {
 	var points = [];
 	var avg = response.stats.avg;
 	var avgC = response.constStats.avg;
-	var totalDays=response.data.page.length;
-	if(response.color == "red" && response.totalPages != 0){
+	var totalDays = response.data.page.length;
+	if (response.color == "red" && response.totalPages != 0) {
 		//Unifinished book!
-		totalDays =  Math.ceil(response.totalPages/avg);
+		totalDays = Math.ceil(response.totalPages / avg);
 		//avg = avgC;
 	}
 
@@ -95,58 +95,58 @@ function drawData(bookHash, response) {
 	data.addColumn('number', 'Average');
 
 	points.push([ 0, 0 , 0, 0, avg]);
-	for (var i = 0; i < totalDays ; i++) {
-		points.push([ (i + 1), response.data.page[i], Math.round(avg*(i+1)),  response.data.delta[i] , Math.round(avg*10)/10]);
+	for (var i = 0; i < totalDays; i++) {
+		points.push([ (i + 1), response.data.page[i], Math.round(avg * (i + 1)), response.data.delta[i] , Math.round(avg * 10) / 10]);
 	}
 
 	data.addRows(points);
 
 	var options = {
-		width: 500,
-		height: 240,
-		title: response.title + ' : '  + response.finishDate + ' ' + response.calculatedFinishDate,
-		titleTextStyle: {color: response.color},
-		hAxis: {title: 'Day', gridlines: {count: ( totalDays + 1) }, minValue: 0, maxValue: ( totalDays - 1 ) },
-		vAxis: {title: response.title + " ( "+response.totalPages + " )", minValue: 0, maxValue: 100 },
-		lineWidth: 1,
-		legend: 'none'
+		width:500,
+		height:240,
+		title:response.title + ' : ' + response.finishDate + ' ' + response.calculatedFinishDate,
+		titleTextStyle:{color:response.color},
+		hAxis:{title:'Day', gridlines:{count:( totalDays + 1) }, minValue:0, maxValue:( totalDays - 1 ) },
+		vAxis:{title:response.title + " ( " + response.totalPages + " )", minValue:0, maxValue:100 },
+		lineWidth:1,
+		legend:'none'
 	};
 
 	var chart = new google.visualization.LineChart(document.getElementById("chart_" + bookHash));
 	chart.draw(data, options);
 }
 
-function setBookDataTable(bookHash, response){
+function setBookDataTable(bookHash, response) {
 	var style = " style='border-bottom : hidden;'";
 	var il = document.getElementById(bookHash);
 	var html = "<table class='tableList '>" +
-				"<tbody>" +
-				"<tr>" +
-				"<th colspan='3'>" +
-				"<span class='headerBookTitle'> <a href='#"+bookHash+"_chart'>" + response.title + /**/ "[" + response.totalPages + "]"+/**/"</a></span>" +
-				"<span class='toRight'>" + /**/response.finishedOn +/**/ "</span>" +
-				"</th>" +
-				"</tr>" +
-				"</tbody>" +
-				"<tbody>";
+		"<tbody>" +
+		"<tr>" +
+		"<th colspan='3'>" +
+		"<span class='headerBookTitle'> <a href='#" + bookHash + "_chart'>" + response.title + /**/ "[" + response.totalPages + "]" + /**/"</a></span>" +
+		"<span class='toRight'>" + /**/response.finishedOn + /**/ "</span>" +
+		"</th>" +
+		"</tr>" +
+		"</tbody>" +
+		"<tbody>";
 
 	var numUpdates = response.data.page.length;
-	for (var i = numUpdates-1; i >= 0; i--) {
+	for (var i = numUpdates - 1; i >= 0; i--) {
 		var date = response.updatesDates[numUpdates - i - 1];
 
-		if(i == 0){ //remove last line
-			html += ("<tr" + style +">");
-		}else{
+		if (i == 0) { //remove last line
+			html += ("<tr" + style + ">");
+		} else {
 			html += ("<tr  >");
 		}
 
 		//html += ("<td class='dateCol'>" + date  + "</td>");
-		html += ("<td class='dateCol'>" + new Date(date * 1000).toUTCString().substr(0,16) + "</td>");
+		html += ("<td class='dateCol'>" + new Date(date * 1000).toUTCString().substr(0, 16) + "</td>");
 		html += ("<td class='pageCol'>" + response.data.page[i] + "</td>");
 		html += ("<td class='pageCol'>" + response.data.delta[i] + "</td>");
 		html += "</tr>";
 	}
 	html += "</tbody>" +
-			"</table>";
+		"</table>";
 	il.innerHTML = html;
 }
